61222c3bad9a771eb097b6ef06862b7db03e8f3e
Mad <mkurchitskiy@hawk.iit.edu>
2024-03-21 21:56:57 -0500

    Lab 3 Javascript finished, calculator works

--- lab_3/js/index.js
+++ lab_3/js/index.js
@@ -1,21 +1,39 @@
 
-// Validate Bill Total Input for numbers
-let BillTotalInput = document.querySelector('input[name="bill-total"]');
+// Get content from form and store in variables
+document.addEventListener('DOMContentLoaded', function() {
+    const billTotalInput = document.querySelector('input[name="bill-total"]');
+    const tipSlider = document.querySelector('input[name="tip-slider"]');
+    const tipPercentageDisplay = document.querySelector('input[name="tip-percentage"]');
+    const tipAmountDisplay = document.querySelector('input[name="tip-amount"]');
+    const totalBillDisplay = document.querySelector('input[name="total-bill-w-tip"]');
 
-BillTotalInput.addEventListener("input", validateBillTotal);
-
-function validateBillTotal() {
-    if (!/^\d*\.?\d*$/.test(BillTotalInput.value)) {
-        alert("Please enter numbers only.");
-        // Clears the invalid input immediately
-        BillTotalInput.value = '';
+    function validateBillTotal(input) {
+        if (/[^0-9.]/.test(input.value)) {
+            alert("Please enter numbers only.");
+            input.value = input.value.replace(/[^0-9.]/g, '');
+        }
     }
-}
 
-// Get tip from slider and log it into tip percentage field
-function viewTipPercentage() {
-    const BillTotal = parseFloat(BillTotalInput.value);
-    const viewTipPercentage = parseInt(tipSlider.value, 10);
+    // Get tip from slider and log it into tip percentage field
+    function calculate() {
+        const billTotal = parseFloat(billTotalInput.value) || 0;
+        const tipPercentage = parseInt(tipSlider.value, 10) || 0;
 
-    
-}
+        tipPercentageDisplay.value = tipPercentage + '%';
+        const tipAmount = (billTotal * tipPercentage) / 100;
+        tipAmountDisplay.value = tipAmount.toFixed(2);
+
+        const totalBill = billTotal + tipAmount;
+        totalBillDisplay.value = totalBill.toFixed(2);
+    }
+
+    // Validate Bill Total Input for numbers
+    billTotalInput.addEventListener("input", function() {
+        validateBillTotal(this);
+        calculate();
+    });
+
+    tipSlider.addEventListener('input', function(){
+        calculate();
+    });
+});
